d48bdb8ac183ae38e013fef3ec5e60a1e285315f
itforget CORDEIRO <itforget11@gmail.com>
2024-04-17 20:18:49 +0000

    fix: get user

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,46 +4,56 @@ import type User from "../user/user";
 let listOfUsers: Array<User> = [];
 
 export default class UserController {
-   addUser(req: Request, res: Response) {
-    const { id, name, password, dateBirth, email} = <User>req.body;
-    const newUser: User = { 
-        id,
-        name,
-        password,
-        dateBirth,
-        email
+  addUser(req: Request, res: Response) {
+    const { id, name, password, dateBirth, email } = <User>req.body;
+    const newUser: User = {
+      id,
+      name,
+      password,
+      dateBirth,
+      email,
     };
-    listOfUsers.push(newUser)
-    return res.status(201).json({ message: "New user created!" })
+    listOfUsers.push(newUser);
+    return res.status(201).json({ message: "New user created!" });
   }
-  listUsers (req: Request, res: Response) {
-    return res.status(200).json(listOfUsers);
-    }
 
-  updateUser (req: Request, res: Response) {
+  listUsers(req: Request, res: Response) {
+    return res.status(200).json(listOfUsers);
+  }
+
+  getUserById(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = listOfUsers.find((user) => user.id === Number(id));
+    if (!user) {
+      return res.status(404).json({ erro: "User not found" });
+    }
+    return res.status(200).json(user);
+  }
+
+  updateUser(req: Request, res: Response) {
     const { id } = req.params;
     const { name, password, dateBirth, email } = req.body as User;
     const user = listOfUsers.find((user) => user.id === Number(id));
-    if (!user) { 
-    return res.status(404).json({ erro: "User not found" });
+    if (!user) {
+      return res.status(404).json({ erro: "User not found" });
     }
-    
+
     user.name = name;
     user.dateBirth = dateBirth;
     user.password = password;
-    user.email = email
+    user.email = email;
 
     return res.status(200).json(user);
-    }
-  
+  }
+
   deleteUser(req: Request, res: Response) {
-    const { id } = req.params
-    const user = listOfUsers.find((user) => user.id === Number(id))
+    const { id } = req.params;
+    const user = listOfUsers.find((user) => user.id === Number(id));
     if (!user) {
-        return res.status(404).json({ erro:"User not found" })
+      return res.status(404).json({ erro: "User not found" });
     }
-    const index = listOfUsers.indexOf(user)
-    listOfUsers.slice(index, 1)
-    return res.status(200).json({ mensagem: "User delete complete" })
+    const index = listOfUsers.indexOf(user);
+    listOfUsers.slice(index, 1);
+    return res.status(200).json({ mensagem: "User delete complete" });
   }
 }
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -7,6 +7,7 @@ const userController = new UserController();
 
 router.post("/", userController.addUser);
 router.get("/", userController.listUsers);
+router.get("/:id", userController.getUserById)
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
